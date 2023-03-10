35c9b497fc17a7e64bd32598ecc0f45ec2a4ae78
Nathan <nathan@neopolis.io>
2023-03-10 20:12:55 +0100

    Add env

--- server/build/index.js
+++ server/build/index.js
@@ -1,3 +1,30 @@
 "use strict";
-console.log('Hello World!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const firebase_admin_1 = __importDefault(require("firebase-admin"));
+const app = (0, express_1.default)();
+firebase_admin_1.default.initializeApp();
+const db = firebase_admin_1.default.firestore();
+app.use(express_1.default.json());
+app.get('/users', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    db.collection('users').get().then(snapshot => {
+        snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+        });
+    });
+    res.send('Hello World!');
+});
+app.post('/users', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    // res.send ( 'Hello World!' );
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,33 @@
-import express from 'express' ;
-import admin from 'firebase-admin' ;
+import express from "express";
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
+
 admin.initializeApp();
 
+const app = express();
 const db = admin.firestore();
 
-app.use (express.json());
+app.use(express.json());
 
+app.get("/users", async (req, res) => {
+  const users = await db
+    .collection("users")
+    .get()
+    .then((query) => query.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+  console.log(users);
 
-app.get ( '/users' , (req, res) => {
-    const body = req.body;
-    console.log (body);
+  res.send(users);
+});
 
-    db.collection ( 'users' ).get().then (snapshot => {
-        snapshot.forEach (doc => {
-            console.log (doc.id, '=>', doc.data());
-            });})
+app.post("/users", (req, res) => {
+  const body = req.body;
+  console.log(body);
 
-    res.send ( 'Hello World!' );
-    });
+  // res.send ( 'Hello World!' );
+});
 
-app.post ( '/users' , (req, res) => {
-    const body = req.body;
-    console.log (body);
-
-
-    // res.send ( 'Hello World!' );
-    });
-
-    
-app.listen ( 3000 , () => {
-    console.log ( 'Server is running on port 3000' );
-  });
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
